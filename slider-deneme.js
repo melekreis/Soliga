5283cc80e74b9547147cce30b4bb0329efe51cc4
melekreis <160604319+melekreis@users.noreply.github.com>
2024-02-24 17:21:40 +0300

    preface Slider-2

--- slider-deneme.js
+++ slider-deneme.js
@@ -1,39 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector('.slider');
-    const boxes = document.querySelectorAll('.slider > div');
-    const boxWidth = boxes[0].offsetWidth;
-    const totalWidth = boxes.length * boxWidth;
-    let currentIndex = 0;
-    let intervalId;
-  
-    // Slider'ı başa dönerek tekrar etmesini sağlayan fonksiyon
-    function loop() {
-      if (currentIndex === boxes.length) {
-        currentIndex = 0;
-        slider.style.transition = 'none';
-        slider.style.transform = `translateX(0)`;
-        setTimeout(() => {
-          slider.style.transition = '';
-        }, 10);
-      }
-    }
-  
-    function startAutoSlide() {
+  const slider = document.querySelector('.slider');
+  const boxes = document.querySelectorAll('.slider > div');
+  const boxWidth = boxes[0].offsetWidth;
+  const totalWidth = boxes.length * boxWidth;
+  let currentIndex = 0;
+  let intervalId;
+
+  function startAutoSlide() {
       intervalId = setInterval(() => {
-        currentIndex++;
-        slider.style.transform = `translateX(${-currentIndex * boxWidth}px)`;
-        loop(); // Her slaydın sonunda başa dönmesi için kontrol
-      }, 2000); // Her 2 saniyede bir slaydı kaydır
-    }
-  
-    startAutoSlide(); // Otomatik kaydırma işlemini başlat
-  
-    slider.addEventListener('mouseenter', () => {
+          currentIndex++;
+          slider.style.transform = `translateX(${-currentIndex * boxWidth}px)`; // Slider'ı kaydır
+
+          if (currentIndex === boxes.length -3) { // Son div ekrana girer girmez
+              currentIndex = 0; // İlk div'e dön
+              slider.style.transition = 'none'; // Geçiş efektini kapat
+              slider.style.transform = `translateX(0)`; // Slider'ı başa döndür
+              setTimeout(() => {
+                  slider.style.transition = ''; // Geçiş efektini tekrar aç
+              }, 10);
+          }
+      }, 1400);
+  }
+
+  startAutoSlide(); // Otomatik kaydırma işlemini başlat
+
+  slider.addEventListener('mouseenter', () => {
       clearInterval(intervalId); // Fare slider üzerine geldiğinde otomatik kaydırma işlemini durdur
-    });
-  
-    slider.addEventListener('mouseleave', () => {
-      startAutoSlide(); // Fare slider üzerinden ayrıldığında otomatik kaydırma işlemini tekrar başlat
-    });
   });
-  
+
+  slider.addEventListener('mouseleave', () => {
+      startAutoSlide(); // Fare slider üzerinden ayrıldığında otomatik kaydırma işlemini tekrar başlat
+  });
+});
