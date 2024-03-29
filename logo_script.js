b88426344576113b7817565c5b6687edcf29c463
melekreis <160604319+melekreis@users.noreply.github.com>
2024-03-30 02:22:04 +0300

    asd

--- logo_script.js
+++ logo_script.js
@@ -1,8 +1,7 @@
 document.getElementById('rotatingImage').addEventListener('mouseenter', function() {
-    this.style.animation = 'rotate 1s linear infinite alternate';
-  });
-  
-  document.getElementById('rotatingImage').addEventListener('mouseleave', function() {
-    this.style.animation = ''; // Mouse çıkınca animasyonu kaldırıyoruz
-  });
-  
+  this.style.animation = 'rotate 1s linear infinite alternate';
+});
+
+document.getElementById('rotatingImage').addEventListener('mouseleave', function() {
+  this.style.animation = ''; // Mouse çıkınca animasyonu kaldırıyoruz
+});
