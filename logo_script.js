22efc77baaaf43fd70b3e2a607e2d5f477ccad3a
melekreis <160604319+melekreis@users.noreply.github.com>
2024-05-06 11:31:49 +0300

    asdsa

--- logo_script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('rotatingImage').addEventListener('mouseenter', function() {
-  this.style.animation = 'rotate 1s linear infinite alternate';
-});
-
-document.getElementById('rotatingImage').addEventListener('mouseleave', function() {
-  this.style.animation = ''; // Mouse çıkınca animasyonu kaldırıyoruz
-});
